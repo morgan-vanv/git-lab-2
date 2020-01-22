56378bae80f9cedfe4e363b18ccb6bf8fea40cd1
Morgan.VanV <morganvanv@gmail.com>
2020-01-22 18:51:33 -0500

    Oh boy do I love GIT

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Morgan Van Valkenburgh
+ *        Date: January 22nd 2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!! It feels good to be back." << endl;
+    /*add code*/
+    return 0;
+}// main
